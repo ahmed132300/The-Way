325c6844290e88334d3eb451d4ff4519f7ea1c63
ahmed132300 <adesigner132@gmail.com>
2025-01-30 14:40:55 +0300

    ا

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Header from "./Components/Header";
 import Blog from "./Components/blog";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Quran from "./pages/Quran";
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/home" element={<Home/>}>
+      </Route>
+      <Route path="/quran" element={<Quran/>}>
       </Route>
     </Routes>
     </BrowserRouter>
--- src/pages/Quran.jsx
+++ src/pages/Quran.jsx
@@ -2,7 +2,7 @@ import "./home.css";
 import Header from "../Components/Header";
 import Blog from "../Components/blog";
 import Imgs from "../Components/Imgs";
-function Home() {
+function Quran() {
   let images = [
     "https://www.shutterstock.com/image-photo/holy-al-quran-written-arabic-600nw-2145394919.jpg",
    "https://www.mominoun.com/picture/2015-10/reel/56321c0ee24fe1256500255.jpg",
@@ -19,29 +19,15 @@ function Home() {
         <div className="Blogs">
           <Blog
             img="https://www.shutterstock.com/image-photo/holy-al-quran-written-arabic-600nw-2145394919.jpg"
-            name="تفسير القران"
-            desc="جميع سور القران مفسرة جاهزة"
-            link="/quran"
-            btn="تفسير القران"
-          />
-          <Blog
-            img="https://cdn.alweb.com/thumbs/alseerahalnabaweyah/article/fit604x453/1/%D9%87%D8%AC%D8%B1%D8%A9-%D8%A7%D9%84%D8%B1%D8%B3%D9%88%D9%84-%D9%85%D9%86-%D9%85%D9%83%D8%A9-%D8%A5%D9%84%D9%89-%D8%A7%D9%84%D9%85%D8%AF%D9%8A%D9%86%D8%A9.jpg"
-            name="السيرة النبوية"
-            desc="سيرة النبي صلي الله عليه وسلم من ولادته الي مماته"
-            link="/sirah"
-            btn="السيرة النبوية"
-          />
-          <Blog
-            img="https://www.mominoun.com/picture/2015-10/reel/56321c0ee24fe1256500255.jpg"
-            name="الفقه الميسر "
-            desc="جميع شروحات الفقه مع الكتب"
-            link="/fiqh"
-            btn="الفقه الميسر"
+            name=" سورة الفاتحة"
+            desc={`تفسير سورة الفاتحة من كتاب الظلال`}
+            link="/alfateha"
+            btn="تفسير سورة الفاتحة"
           />
         </div>
       </div>
-      </>
+    </>
   );
 }
 
-export default Home;
+export default Quran;
